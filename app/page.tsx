e8ced67314bc001ce48ce91d7820547f5fd95056
kapureka6174 <mitsu.n.0109@gmail.com>
2024-01-01 15:42:18 +0900

    style(ui): add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
-  return <p className='text-red-300'>hello twich clone!</p>;
+  return <Button size='lg'>Click</Button>;
 }
